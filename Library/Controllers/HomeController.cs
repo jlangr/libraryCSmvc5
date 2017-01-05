baf1caf264d58cef959a5a70affb6a545e0aa9b9
Jeff Langr <jeff@langrsoft.com>
2017-01-05 14:18:00 -0700

    some simple web page cleanup

--- Library/Controllers/HomeController.cs
+++ Library/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Library.Controllers
 {
@@ -15,15 +11,13 @@ namespace Library.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "About the Library System";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Message = "Details";
             return View();
         }
     }
