378a4ce36c2686cf375fa76a6d3accd3d45a8dd9
Jeff Langr <jeff@langrsoft.com>
2017-01-06 10:54:14 -0700

    added a slow repo test

---
+++ LibraryTests/LibraryTest/Models/Repositories/HoldingRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using Library.Models.Repositories;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryTests.LibraryTest.Models.Repositories
+{
+    [TestFixture]
+    public class HoldingRepositoryTest
+    {
+
+        [Test]
+        public void FindByBarcodeReturnsNullWhenNotFound()
+        {
+            var repo = new HoldingRepository();
+
+            Assert.That(repo.FindByBarcode("AA:1"), Is.Null);
+        }
+    }
+}
